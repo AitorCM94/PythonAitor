1a6a622c5a9405e2eec98c904e13e3d715071065
Aitor Cerdán Mañé <82872545+AitorCM94@users.noreply.github.com>
2021-04-26 14:36:38 +0200

    sentencias de repetición_índices

---
+++ Ejercicio3.py
@@ -0,0 +1,14 @@
+#tambla de multiplicar.
+num = input("Dime un numero: ")
+num = int(num)
+
+#Ejemplo1
+"""mult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for n in mult:
+    print(f"{n} x {num} = {n*num}")
+    """
+
+#Ejemplo2
+for n in range (1,11,1):
+    print(f"{num} x {n} = {num*n}")
