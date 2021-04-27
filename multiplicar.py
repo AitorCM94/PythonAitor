535d71784df83bb08d2c0119764fa99e98cb549c
Aitor Cerdán Mañé <82872545+AitorCM94@users.noreply.github.com>
2021-04-27 20:14:38 +0200

    fin_clase_26

--- multiplicar.py
+++
@@ -1,8 +0,0 @@
-numoFor = input("La tabla del: ")
-
-if(numFor.isdigit() == True):
-    print()
-    for numero in range (11):
-        print(f"{numFor} x {numero:2.0f} = {numFor*numero:2.0f}")
-else:
-    print(f"{numero}, no es un número.")
