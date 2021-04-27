eec543bf7d67ce42f0c02aec4eb2617e4b3ab4af
Aitor Cerdán Mañé <82872545+AitorCM94@users.noreply.github.com>
2021-04-27 14:03:37 +0200

    finClase

---
+++ Clase27_04/listas.py
@@ -0,0 +1,29 @@
+numero = 0
+array = []
+
+contador = 0
+while (contador < 10):
+    numero = int(input("Escribe numeros: "))
+    if (type(numero) is int):
+        array.append(numero)
+        contador += 1
+    else:
+        print("No es correcto.")
+print(array)
+
+suma = 0
+for s in array:
+    suma += s
+print(suma)
+
+media = suma / len(array)
+print (media)
+
+#Pintar cuantos pares hay
+pares = 0
+for p in array:
+    pares = p / 2
+    if (pares is int):
+        prinet(pares.count())
+
+
--- Clase27_04/while_numeros.py
+++ Clase27_04/while_numeros.py
@@ -1,13 +1,17 @@
 import random
 #while dime un numero, si es es numero has acertado, si no seguimos ejecutando
 
-numero1 = random.randint(1, 2)
+numero1 = random.randint(1, 10)
 numero2 = 0
 
 while(numero1 != numero2): #LA CONDICIÓN ES LO MAS IMPORTANTE
     numero2 = int(input("Dime un numero: "))
     if (numero1 != numero2):
         print("No has acertado")
+        if(numero2 < numero2):
+            print(f"Te faltan {numero2 - numero1} numeros.")
+        else:
+            print(f"Te has pasado por {numero1 - numero2} numeros.")
     else:
         print("Has acertado")
     
