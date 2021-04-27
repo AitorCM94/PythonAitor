f53c5ade387aaa57c6b03ecc99f8036ee0fd1c4a
Aitor Cerdán Mañé <82872545+AitorCM94@users.noreply.github.com>
2021-04-27 17:30:42 +0200

    pares

--- Clase27_04/listas.py
+++ Clase27_04/listas.py
@@ -3,27 +3,26 @@ array = []
 
 contador = 0
 while (contador < 10):
-    numero = int(input("Escribe numeros: "))
+    numero = int(input("Escribe un número: "))
     if (type(numero) is int):
         array.append(numero)
         contador += 1
     else:
         print("No es correcto.")
-print(array)
+print(f"Núeros introducidos: {array}.")
 
 suma = 0
 for s in array:
-    suma += s
-print(suma)
+    suma += s #se van acumulando uno encima de otro
+print(f"Suma total: {suma}.")
 
 media = suma / len(array)
-print (media)
+print (f"La media es: {media}.")
 
-#Pintar cuantos pares hay POR HACER
 pares = 0
 for p in array:
-    pares = p / 2
-    if (pares is int):
-        prinet(pares.count())
+    if (p%2 == 0):
+        pares += 1
+print(f"{pares} números pares.")
 
 
