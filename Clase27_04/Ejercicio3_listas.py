6983d02ad2484825045a7c514010563889e739ab
Aitor Cerdán Mañé <82872545+AitorCM94@users.noreply.github.com>
2021-04-28 11:21:19 +0200

    primer

--- Clase27_04/Ejercicio3_listas.py
+++ Clase27_04/Ejercicio3_listas.py
@@ -2,7 +2,7 @@ numero = 0
 array = []
 
 contador = 0
-while (contador < 10):
+while (contador < 10): #Podriamso usar la funcion len() para indicar que dure hasta que pase por todos los elementos de la array.
     numero = int(input("Escribe un número: "))
     if (type(numero) is int):
         array.append(numero)
@@ -11,7 +11,7 @@ while (contador < 10):
         print("No es correcto.")
 print(f"Núeros introducidos: {array}.")
 
-suma = 0
+suma = 0 #podemos juntar todas las operaciones con un for -> if -> else // Y después meter todo en el WHILE.
 for elemento in array:
     suma += elemento #se van acumulando uno encima de otro
 print(f"Suma total: {suma}.")
@@ -24,3 +24,9 @@ for elemento in array:
     if (elemento%2 == 0):
         pares += 1
 print(f"{pares} números pares.")
+
+impares = 0
+for elemento in array:
+    if (elemento%2 != 0):
+        impares += 1
+print(f"{impares} números impares.")
---
+++ arraysJSON.py
@@ -0,0 +1,8 @@
+import json
+
+# Pasar un objeto a JSON_
+citricos = ["naranja", "limón", "pomelo", "lima"]
+listaJSON = json.dumps(citricos)
+
+print(citricos)
+print(listaJSON)
