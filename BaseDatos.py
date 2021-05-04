6d37173cb882b06a9420a3d652d33bfbc32ea29c
Aitor Cerdán Mañé <82872545+AitorCM94@users.noreply.github.com>
2021-05-04 20:53:09 +0200

    next

--- BaseDatos.py
+++ BaseDatos.py
@@ -1,5 +1,56 @@
-import pickle
+from pymongo import MongoClient #Objeto cliente que nos permite la conexión con la base de datos.
+from pprint import pprint
 
-file = open(".\\SEMANA2\\Clase03_05\\fichero.json","rt", encoding="UTF-8") #Modo lectura.
-dataJSON = file.read() #Leemos todo el contenido del fichero. Variable <class 'str'>.
-file.close()
+#Establecer conexión con MongoDB (motor de base de datos):
+#Pasar direccion ip o nombre del servidor:
+client = MongoClient('mongodb://localhost:27017')
+#client = MongoClient('localhost',27017)
+
+#Estado del servidor (motor de base de datos):
+db = client.admin #volcar sobre la variable un objeto que representa la base de datos admin.
+status = db.command("serverStatus") #.command permite ejecutar comandos. Pintamos el estado del servidor (toda la información del servidor).
+#print(status) #Datos inmanegables
+#pprint(status) #Pintamos los datos con pprint -> más manejable.
+
+
+##Trabajando con Datos:
+#Seleccionar la base de datos:
+northwindDB = client.Northwind #Volcar la base de datos (nombre de la base de datos) en la variable. Notación de objeto.
+#northwindDB = client['Nortwind'] #Lo mismo. Lo trata como una clave. Notación de diccionario.
+
+#Listar las colecciones de la base de datos. Recorrerlas para acceder a los datos.
+#print(northwindDB.list_collections()) #Lista de objetos.
+print(northwindDB.list_collection_names()) #Lista string. Podemos recorrerlas con el for.
+#Con un filter, si nos devuelve 0 eementos no existe, si devuelve uno si. Más eficiente, preguntar si un alfanumerico se encuentra dentro de la lista.
+
+#Seleccionar una coleccion de la base de datos.
+#Diferentes fórmulas: 
+#collection = northwindDB['customers'] #Notación de diccionario. Nombre de la colección.
+#collection = northwindDB.customers #con notacion de objeo. Igual que seleccionar la base de datos.
+#Siempre con la variable que contiene la base de datos. Si no:
+collection = client.Northwind.customers 
+#collection = client['Northwind']['customers']
+
+#Uso de métodos para acceder a la colección: 
+result = collection.estimated_document_count() #Para contar el número de registros (documentos).
+#print(result)
+
+#BÚSQUEDA de documentos:
+#result = collection.find_one() #Primer elemento coincidente.
+result = collection.find({'Country': 'USA'}, {'Country': 'Mexico'}).sort('City') #Todos los elementos coincidentes. Devuelve una coleccion que podemos recorrer.
+
+#print("Número de documentos: ", result.count())
+print("Número de documentos: ", collection.count_documents({'Country': 'USA'}))
+print("Datos por leer: ", result.alive)
+#for d in result: #Recorremos todos los registros encontrados
+#    pprint(d)
+while (result.alive == True):
+    pprint(result.next())
+
+#El filtro de busquedo lo pasamos como un objeto dentro de la función .find({objeto}) sobre el elemento que queremos que realize búsqiedas
+#El metodo .next() pinta el usuario. Posiciona el cursor en el siguiente registro.
+# .alive -> Para ver las propiedades del cursor. Muy interesante para recorrer los registros con un while -> Siempre que devuelva true podemos ejecutar el nex.
+
+#Es micho más eficiente contar los elementos en las bases de datos. Funciones que hagan calculos en las bases de datos en vez de en local.
+# .sort('[elementos]') ordenar los elementos.
+
--- SEMANA1/Clase29_04/2FicherosBytesBinarios.py
+++ SEMANA1/Clase29_04/2FicherosBytesBinarios.py
@@ -1,23 +1,23 @@
+import pickle #Objeto que nos permite gravar o leer en binario.
 
-"""
-#Métodos para pasar un texto a bytes. 
+#Métodos para pasar un texto a bytes: 
 bytes1 = "En un lugar de la Mancha...".encode() #Bytes
 bytes2 = b"En un lugar de la Mancha..." #Bytes
-bytes3 = bytes("En un lugar de la Mancha...", "utf-8") #Bytes
+bytes3 = bytes("En un lugar de la Mancha...", "utf-8") #Bytes. utf-8 -> Especifica el encoding del texto (como esta codificado).
 print(type(bytes1)) #Python pinta el texto con una b delante para indicar que esta en formato de bytes.
 
 for b in bytes3:
-    print(b, end=' ') #Pinta cada caracter en formato byte.
-"""
+    print(b, end=' ') #Pinta cada caracter en formato byte. end=' ' Para que pinte en una sola línea y separados por espacios.
+print("")
+print(bytes3.hex()) #Representación de bytes a hexadecimal. Cada dos carácteres es la representación de uno en formato de bytes (decimal).
 
-#MODO BINARIO:
-import pickle
 """
 #1. Abrimos el fichero en modo escritura, binario. Si el fichero no existe, crea uno nuevo:
 fichero = open(".\\SEMANA1\\Clase29_04\\2Test.bin", "wb")
 #2. Escribimos en el fichero con la función .dump:
 texto = "En un lugar de la Mancha..."
 pickle.dump(texto, fichero)
+fichero.close()
 """
 """
 #1. Abrimos el fichero en modo lectura, binario:
@@ -25,5 +25,11 @@ fichero = open(".\\SEMANA1\\Clase29_04\\2Test.bin", "rb")
 #2. Leemos la información del fichero con la función .load:
 texto = pickle.load(fichero)
 #print(texto)
-"""
 fichero.close()
+"""
+"""
+#Podemos gravar la información en formato bytes o hexadecimal en un fichero de texto normal:
+fichero = open(".\\SEMANA1\\Clase29_04\\2Test.txt", "wt")
+fichero.write(bytes3.hex())
+fichero.close()
+"""
