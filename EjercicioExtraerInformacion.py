e28ab0e9c5ed526506cc5101cbac206a7f6f6fc0
Aitor Cerdán Mañé <82872545+AitorCM94@users.noreply.github.com>
2021-05-06 11:33:08 +0200

    funciona

---
+++ EjercicioExtraerInformacion.py
@@ -0,0 +1,45 @@
+from itertools import product
+from pymongo import MongoClient
+from pprint import pprint
+
+#Cliente:
+connect = MongoClient('mongodb://localhost:27017/')
+
+#Base de datos:
+db = connect.Northwind
+
+#Colecciones:
+orders = db.orders
+details = db.order_details
+products = db.products
+
+
+##Buscar cuantos productos tenemos:
+#print("Número de productos: ", products.estimated_document_count())
+
+##Buscar y mostrar todos los productos:
+listProducts = products.find() #Devuelve un objeto Cursor de tipo colección (como si fuera una lista) que podemos recorrer:
+#for document in listProducts:
+#    print(f"{document['ProductName']}") #Pinta todos los documentos de la colección.
+#Con un while:
+#cursor = products.find()
+#while(cursor.alive): #Cuando .live is true.
+#    print(cursor.next()['ProductName'])
+
+##Buscar productos con UnitsInStock = 0:
+#pro = list(filter(lambda p: p['UnitsInStock'] == '0', listProducts)) #Buscar los productos de manera local.
+#for p in pro:
+#    print(f"{p['ProductName']}")
+#pNoStock = products.find({'UnitsInStock': '0'}) #Buscar los productos en la base de datos. Más optimizado.
+#for document in pNoStock:
+#    print(document['ProductName'])
+
+##Coste o Valor de nuestro Stock - Producto -> UnitsInStock, UnitPrice:
+pInStock = products.find({'UnitsInStock':{'$ne':'0'}}) #Buscamos los productos que tienen stock no igual a 0.
+valorStock = 0
+for p in listProducts:
+    valorStock += (float(p['UnitPrice']) * float(p['UnitsInStock']))
+
+print(f"\nValor del Stock: {valorStock:1.2f}")
+
+##Con la función de agregación podemos hacer que la base de datos nos de directamente la operación... POR MIRAR!!!
--- SEMANA2/Clase05_05/5DB_Collections.py
+++ SEMANA2/Clase05_05/5DB_Collections.py
@@ -28,13 +28,16 @@ if(pedido != None): #Si el pedido existe. Lo pintamos:
     print(f"")
     #Buscamos el detalle del pedido:
     detalle = details.find({'OrderID' : idPedido}) #Devuelve un cursor que tendremos que recorrer con While:
+    print(f"=================================================================")
+    print(f"  {'Producto':<35} {'Cant. '} {'Precio'} ")
+    print(f"=================================================================")
     while(detalle.alive): #.alive nos dice si es true podemos seguir ejecutando el .next()
         #Buscamos la información del producto:
         linea = detalle.next() #Recorrmos cada línea de la colección de detalles del pedido.
         producto = products.find_one({'ProductID' : linea['ProductID']}) #Buscamos en la colección de products (con el ID) el nombre, para pintarlo.
-        total = int(linea['Quantity']) * float(linea['UnitPrice']) #Calculamos el dinero total del pedido.
+        totalLinea = int(linea['Quantity']) * float(linea['UnitPrice']) #Calculamos el dinero total del pedido.
         #Pintamos la información obtenida:
-        print(f"  {producto['ProductName']:<35} {linea['Quantity']:>6} {linea['UnitPrice']:>8} {total:>8}") 
+        print(f"  {producto['ProductName']:<35} {linea['Quantity']:>6} {linea['UnitPrice']:>8} {totalLinea:>8}") 
         #Tabula la información: Especificar el máximo de espacios de la columna y su alineación (<izquierda, >derecha)
     #Mostramos el importe total de todos los pedidos.
     #importeTotal = 
